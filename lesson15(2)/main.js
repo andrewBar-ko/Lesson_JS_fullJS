a0f58831682d51b17163857c48de668264b1a282
Andrew Barkovets <andrewbarkovets@gmail.com>
2020-11-01 16:15:12 +0300

    feat(main.js): new  feat

--- lesson15(2)/main.js
+++ lesson15(2)/main.js
@@ -1,2 +1,29 @@
 'use strict';
 
+class First {
+    constructor(hello) {
+       this.hello = hello;
+    }
+
+    writeHiPar() {
+        console.log(this.hello);
+    }
+}
+
+class Second extends First {
+    constructor(hello, child) {
+        super(hello);
+        this.child = child;
+    }
+    
+    writeHiPar() {
+        console.log(this.child);
+    }
+
+}
+
+const hi = new First("Привет я метод родителя!");
+const hiChild = new First("А я наследуемый метод!");
+
+setTimeout(() => hi.writeHiPar(), 500);
+setTimeout(() => hiChild.writeHiPar(), 1000);
