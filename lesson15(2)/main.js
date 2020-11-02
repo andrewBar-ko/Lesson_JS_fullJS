6e4bc0dfc626347c2425649f6753cc679c824c3f
Andrew Barkovets <andrewbarkovets@gmail.com>
2020-11-02 12:12:55 +0300

    fix(main.js): new fix

--- lesson15(2)/main.js
+++ lesson15(2)/main.js
@@ -1,29 +1,22 @@
 'use strict';
 
-class First {
-    constructor(hello) {
-       this.hello = hello;
-    }
+function First(hello) {
+    this.hello = hello;
 
-    writeHiPar() {
+    First.prototype.writeHiPar = function() {
         console.log(this.hello);
-    }
+    };
+
 }
 
-class Second extends First {
-    constructor(hello, child) {
-        super(hello);
-        this.child = child;
-    }
-    
-    writeHiPar() {
-        console.log(this.child);
-    }
-
+function Second() {
+   
+    First.prototype.writeHiPar();
+ 
 }
 
 const hi = new First("Привет я метод родителя!");
 const hiChild = new First("А я наследуемый метод!");
 
-setTimeout(() => hi.writeHiPar(), 500);
-setTimeout(() => hiChild.writeHiPar(), 1000);
+hi.writeHiPar();
+hiChild.writeHiPar();
